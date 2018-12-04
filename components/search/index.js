7fb8fbaf55a0739973e1d9cce385cba66a2d1544
wangyongtao <mm18575206484@163.com>
2018-12-04 23:12:40 +0800

    2018年12月4日 23:12:32更新

--- components/search/index.js
+++ components/search/index.js
@@ -1,4 +1,9 @@
 // components/search/index.js
+import {
+    KeywordModel
+} from '../../models/keyword.js';
+
+const keywordModel = new KeywordModel();
 Component({
     /**
      * 组件的属性列表
@@ -11,15 +16,25 @@ Component({
      * 组件的初始数据
      */
     data: {
-
+        historyWords: []
     },
 
+    attached() {
+        const historyWords = keywordModel.getHistory();
+        this.setData({
+            historyWords
+        });
+    },
     /**
      * 组件的方法列表
      */
     methods: {
-        onCancel(event){
-            this.triggerEvent('cancle',{},{})
+        onCancel(event) {
+            this.triggerEvent('cancle', {}, {})
+        },
+        onConfirm(event) {
+            const word = event.detail.value;
+            keywordModel.addToHistory(word);
         }
     }
 })
---
+++ models/keyword.js
@@ -0,0 +1,30 @@
+class KeywordModel {
+    key = 'q'
+    maxLength = 10
+    getHistory() {
+        const words = wx.getStorageSync(this.key);
+        if (!words) {
+            return [];
+        }
+        return words;
+    }
+    getHot() {
+
+    }
+    addToHistory(keyword) {
+        let words = this.getHistory();
+        const has = words.includes(keyword);
+        if (!has) {
+            const length = words.length;
+            if (length >= this.maxLength) {
+                words.pop();
+            }
+            words.unshift(keyword);
+            wx.setStorageSync(this.key, words);
+        }
+    }
+}
+
+export {
+    KeywordModel
+}
